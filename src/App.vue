22706253f31ffc6619069751ffea313afbe5647c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-03-02 17:49:30 +0100

    Create post request to jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -29,11 +29,17 @@ export default {
       this.todos = this.todos.filter(todo => todo.id != id);
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
     }
   },
   created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos')
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
       .then(res => this.todos = res.data)
       .catch(err => console.log(err));
   }
